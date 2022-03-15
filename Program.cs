bd61e23db377dbe4f082dd178c941fd41c456f76
Rodriques <rodriques.perry@gmail.com>
2022-03-14 22:28:46 -0500

    Adding numbers

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+double x = 34.1;
+double y = 2.2;
+double sum = x + y;
 
-Console.WriteLine($"Hello, {args[0]}!");
+Console.WriteLine(sum);
+
+if (args.Length > 0) {
+    Console.WriteLine($"Hello, {args[0]}!");
+} else {
+    Console.WriteLine("Hello, Rodriques!");
+}
+
+
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # gradebook
-# gradebook
-# gradebook
+
